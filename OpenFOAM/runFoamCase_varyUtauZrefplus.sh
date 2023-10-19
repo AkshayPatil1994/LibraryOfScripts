b8600f20f9182256e3f0a84ac5c34fc3a55535cb
Akshay Patil <patil.akshay11235@gmail.com>
2023-10-19 13:59:35 +0200

    Updating runFoamCase_varyUtauZrefplus.sh for bash compatibility

--- OpenFOAM/runFoamCase_varyUtauZrefplus.sh
+++ OpenFOAM/runFoamCase_varyUtauZrefplus.sh
@@ -2,7 +2,7 @@
 
 # User input data for the simulations
 nprocs=24
-totalIterations=2
+totalIterations=10000
 # Specify the values of utau and zplusref to be used in the simulations
 utauarray=("2e-3" "4e-3" "6e-3" "8e-3" "1e-2" "1.5e-2" "2e-2" "2.5e-2" "3e-2" "5e-2")
 zplusref=("200" "400" "600" "800" "1000" "1500" "2000" "2500" "3000" "5000")
@@ -15,11 +15,11 @@ lengthz=${#zplusref[@]}
 lengthu=${#utauarray[@]}
 # Ensure that the arrays are of same size
 if [ ${lengthz} -eq ${lengthu} ]; then
-    echo "--------------------------------------------------------"
+    echo -e "${GREEN}--------------------------------------------------------${RESET}"
     echo " This script runs $lengthz cases using simpleFoam"
     echo -e " Number of processors: ${RED} $nprocs ${RESET}"
     echo -e " Total Iterations per simulation: ${RED} $totalIterations ${RESET}"
-    echo "--------------------------------------------------------"
+    echo -e "${GREEN}--------------------------------------------------------${RESET}"
     echo "		*	*	*"
 else
     echo "The size of the utauarray is not equal to zplusref!....."
@@ -28,11 +28,23 @@ else
 fi
 # Measure start time of the script
 startTime=`date +%s`
+# Query and change the number of iterations in system/controlDict
+echo "Editing the endTime and writeInterval in system/controlDict...."
+#sed_command="s/endTime\(.*endTime\)/endTime\endTime	${totalIterations};/"
+#sed_command="s/endTime[[:space:]]*[0-9]*;/endTime         ${totalIterations};/"
+#sed_command="0,/endTime[[:space:]]*[0-9]*;/s//endTime         ${totalIterations};/"
+sed_command="s/^\(endTime[[:space:]]*\)[0-9]*;/\1${totalIterations};/"
+eval "sed -i \"$sed_command\" system/controlDict"
+sed_command="s/writeInterval[[:space:]]*[0-9]*;/writeInterval         ${totalIterations};/"
+eval "sed -i \"$sed_command\" system/controlDict"
+cat system/controlDict | grep "endTime"
+cat system/controlDict | grep "writeInterval"
+echo "	*	*	*"
 # Loop over all the array input to run the cases one by one
 for ((i=0; i<${#zplusref[@]}; i++)); do
     # Query simulation start time
     simStartTime=`date +%s`
-    echo "--------------------------------------------------------"
+    echo -e "${GREEN} -------------------------------------------------------- ${RESET}"
     echo -e "Now running case ${RED} $((i+1)) ${RESET} with  ${RED} Rek = ${zplusref[$i]} ${RESET}"
     # Construct the sed command dynamically
     sed_command="s/\\(ustar[[:space:]]*\\)[0-9.]*e-[0-9]*/\\1${utauarray[$i]}/"
@@ -44,15 +56,16 @@ for ((i=0; i<${#zplusref[@]}; i++)); do
     echo "I/O check that the right values are changed in 0/include/ABLConditions..."
     cat 0/include/ABLConditions | grep "Friction velocity"
     cat 0/include/ABLConditions | grep "Reference wall units"
+    # Decompose the case again
+    echo "Decomposing the mesh for parallel workload for case $(($i+1))...."
+    decomposePar -force > logs/decomposePar.log
     # Set the name of the logfile to log the run I/O
     logfile="logs/run_${zplusref[$i]}.log"
-    echo "Simulation I/O logged to file $logfile"
-    # Decompose the case again
-    decomposePar -force 2>&1 logs/decomposePar.log
-    # Run the case
-    mpirun -np $nprocs simpleFoam -parallel 2>&1 "$logfile"
+    echo "Running the case -- Simulation I/O logged to file $logfile"    # Run the case
+    mpirun -np $nprocs simpleFoam -parallel > "$logfile"
     # Reconstruct the simulation
-    reconstructPar -latestTime 2>&1 logs/reconstruct.log
+    echo "Reconstructing the latest time....."
+    reconstructPar -latestTime > logs/reconstruct.log
     # Move the results to a results folder
     outputFolderName="results/SF${zplusref[$i]}"
     echo -e "Results for this case now moved to -- ${RED} $outputFolderName ${RESET}"
@@ -65,7 +78,7 @@ done
 # Measure start time of the script
 endTime=`date +%s`
 runtime=$((endTime-startTime))
-echo "--------------------------------------------------------"
-echo "$lengthz cases completed in $runtime seconds............"
-echo "			* * *"
-echo "--------------------------------------------------------"
+echo -e "${GREEN} -------------------------------------------------------- ${RESET}"
+echo -e "$lengthz cases completed in $runtime seconds............"
+echo -e "			* * *"
+echo -e "${GREEN} -------------------------------------------------------- ${RESET}"
