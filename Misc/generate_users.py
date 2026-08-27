63fabaf726ef0fd7c21a57aaa83acd16f5ceeb45
Akshay Patil <patil.akshay11235@gmail.com>
2026-08-27 11:37:50 +0200

    Add user generation script with the best usernames

---
+++ Misc/generate_users.py
@@ -0,0 +1,51 @@
+import random
+import string
+
+# Number of users to generate
+num_users = 12  # You can change this
+
+# Lists of funny adjectives and nouns
+adjectives = [
+    'funky', 'squishy', 'wobbly', 'sassy', 'bouncy',
+    'zesty', 'grumpy', 'fluffy', 'chunky', 'jazzy',
+    'snarky', 'quirky', 'jolly', 'spicy', 'goofy'
+]
+
+nouns = [
+    'cat', 'potato', 'pancake', 'taco', 'donut',
+    'pickle', 'monkey', 'waffle', 'nugget', 'cupcake',
+    'penguin', 'sloth', 'otter', 'hamster', 'banana'
+]
+
+def generate_funny_username():
+    """Generates a funny username with adjective + noun + random number."""
+    adj = random.choice(adjectives)
+    noun = random.choice(nouns)
+    number = random.randint(10, 99)
+    return f"{adj}-{noun}{number}"
+
+def generate_random_password(length=10):
+    """Generates a random password with letters, digits, and punctuation."""
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choices(chars, k=length))
+    return password
+
+def create_users(num_users):
+    """Generates a list of user dictionaries with funny usernames and passwords."""
+    users = []
+    for _ in range(num_users):
+        user = {
+            'username': generate_funny_username(),
+            'password': generate_random_password()
+        }
+        users.append(user)
+    return users
+
+# Create users
+users_list = create_users(num_users)
+
+# Print users
+iter = 1
+for user in users_list:
+    print(f"{user['username']} {user['password']}")
+    iter += 1
